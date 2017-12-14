4d9b7f1818598ec388ac4dfdd9435139bf79d025
Alpay, Yasemin (CT DD DS EU TR DA MSA) <yasemin.alpay@siemens.com>
2017-12-14 19:52:52 +0300

    custom form with interest properties

--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -38,5 +38,27 @@ angular.module('app.controllers', [])
                 $scope.content.$save();
             };
 
+            //viewing generated form in html
+            $scope.interestProperties.$promise.then(function (response) {
+                $scope.properties = response;
+
+                var html = "";
+
+                for (var i = 0; i < $scope.properties.length; i++) {
+                    console.log("propertyname", $scope.properties[i].name);
+                    var property = $scope.properties[i];
+                    html += "<div class=\"form-group\">" +
+                        "<label for=\"" + property.name + "\" class=\"col-lg-2 control-label\">" + property.description + "</label>" +
+                        "<div class=\"col-lg-10\">" +
+                        "<input type=\"text\" ng-model=\"content." + property.name + "\" class=\"form-control\" id=\"" + property.name + "\" placeholder=\"" + property.description + "\">" +
+                        "</div>" +
+                        "</div>"
+                }
+
+                $scope.customFields = $sce.trustAsHtml(html);
+
+            });
+
+
         })
     ;
